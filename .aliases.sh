ec26e226272b14a627de484f6ba4205fe06d6a07
Jethro Muller <4126778+AceFire6@users.noreply.github.com>
2023-05-28 11:10:19 +0200

    Fix aliases script

--- .aliases.sh
+++ .aliases.sh
@@ -2,27 +2,26 @@
 # Setup bare git repo alias
 alias conf='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-pushbranch() {
- git push -u origin "$(git rev-parse --abbrev-ref HEAD)"
-}
-
 if command -v nvim &> /dev/null
 then
   # No output during sourcing
+  # https://neovim.io/
   # echo "nvim installed - aliasing vim=>nvim"
   alias vim=nvim
 fi
 
-if command -v nvim &> /dev/null
+if command -v bat &> /dev/null
 then
   # No output during sourcing
+  # https://github.com/sharkdp/bat
   # echo "bat installed - aliasing cat=>bat"
   alias cat=bat
 fi
 
-if command -v nvim &> /dev/null
+if command -v exa &> /dev/null
 then
   # No output during sourcing
+  # https://github.com/ogham/exa
   # echo "exa installed - aliasing exa=>bat"
   alias ls=exa
 fi
